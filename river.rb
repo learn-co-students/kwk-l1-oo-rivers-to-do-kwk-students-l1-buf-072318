a4a29015efe31a1e8817b577738c570fd7434a12
Minka5 <madrakowsky5@gmail.com>
2018-07-30 14:22:35 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,25 @@
 # river.rb
+class River
+  attr_accessor :name, :length, :countries,:discharge
+  
+def initialize(name)
+  @name = name
+  
+end
+
+def flood
+  @discharge =  @discharge * 1.3
+
+end
+
+def dry_up
+  @discharge = @discharge / 2 
+  
+end
+
+
+
+end
+Mississippi = River.new("Mississippi")
+Hudson = River.new ("Hudson")
+
